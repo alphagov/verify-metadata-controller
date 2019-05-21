5c5b2f3b98b130726ece69b7b4838e747847e7ef
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-05-21 17:20:53 +0100

    adds test for general VMC function
    adds a test to ensure Secret, Deployment and Service are created roughly
    as expected

--- pkg/controller/metadata/metadata_controller_test.go
+++ pkg/controller/metadata/metadata_controller_test.go
@@ -17,71 +17,165 @@ limitations under the License.
 package metadata
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 	"time"
 
 	verifyv1beta1 "github.com/alphagov/verify-metadata-controller/pkg/apis/verify/v1beta1"
-	"github.com/onsi/gomega"
+	"github.com/alphagov/verify-metadata-controller/pkg/hsm/hsmfakes"
+	. "github.com/onsi/gomega"
 	"golang.org/x/net/context"
 	appsv1 "k8s.io/api/apps/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-var c client.Client
-
-var expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo", Namespace: "default"}}
-var depKey = types.NamespacedName{Name: "foo", Namespace: "default"}
-
 const timeout = time.Second * 5
 
 func TestReconcile(t *testing.T) {
-	g := gomega.NewGomegaWithT(t)
-	instance := &verifyv1beta1.Metadata{ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"}}
+	ctx := context.Background()
+	g := NewGomegaWithT(t)
 
-	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
-	// channel when it is finished.
+	// Load test certs
+	metadataSigningCert, err := ioutil.ReadFile("test.metadata.signing.crt")
+	g.Expect(err).NotTo(HaveOccurred())
+
+	// Setup a fake env
+	os.Setenv("HSM_IP", "10.0.10.100")
+	os.Setenv("HSM_USER", "hsm-user")
+	os.Setenv("HSM_PASSWORD", "hsm-pass")
+	os.Setenv("HSM_CUSTOMER_CA_CERT_PATH", "test.ca.crt")
+
+	// Setup fake hsm client
+	fakeSignedMetadata := []byte("<signed>FAKE-SIGNED-META</signed>")
+	hsmClient := &hsmfakes.FakeClient{}
+	hsmClient.FindOrCreateRSAKeyPairReturnsOnCall(0, metadataSigningCert, nil)
+	hsmClient.GenerateAndSignMetadataReturnsOnCall(0, fakeSignedMetadata, nil)
+
+	// Setup the Manager and Controller.
 	mgr, err := manager.New(cfg, manager.Options{})
-	g.Expect(err).NotTo(gomega.HaveOccurred())
-	c = mgr.GetClient()
-
-	recFn, requests := SetupTestReconcile(newReconciler(mgr))
-	g.Expect(add(mgr, recFn)).NotTo(gomega.HaveOccurred())
-
+	g.Expect(err).NotTo(HaveOccurred())
+	c := mgr.GetClient()
+	recFn, requests := SetupTestReconcile(newReconciler(mgr, hsmClient), t)
+	g.Expect(add(mgr, recFn)).NotTo(HaveOccurred())
 	stopMgr, mgrStopped := StartTestManager(mgr, g)
-
 	defer func() {
 		close(stopMgr)
 		mgrStopped.Wait()
 	}()
 
-	// Create the Metadata object and expect the Reconcile and Deployment to be created
-	err = c.Create(context.TODO(), instance)
-	// The instance object may not be a valid object because it might be missing some required fields.
-	// Please modify the instance object by adding required fields and then remove the following if statement.
-	if apierrors.IsInvalid(err) {
-		t.Logf("failed to create object, got an invalid object error: %v", err)
-		return
+	// flesh out a metadata object
+	metadataResource := &verifyv1beta1.Metadata{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Spec: verifyv1beta1.MetadataSpec{
+			ID:   "_entities",
+			Type: "proxy",
+			Data: verifyv1beta1.MetadataSigningSpec{
+				EntityID:         "https://mything/",
+				PostURL:          "https://mything/POST",
+				RedirectURL:      "https://mything/Redirect",
+				OrgName:          "orig-org-name",
+				OrgDisplayName:   "Original Org Name",
+				OrgURL:           "https://org1.com",
+				ContactCompany:   "O",
+				ContactGivenName: "jeff",
+				ContactSurname:   "jefferson",
+				ContactEmail:     "jeff@jeff.com",
+			},
+		},
 	}
-	g.Expect(err).NotTo(gomega.HaveOccurred())
-	defer c.Delete(context.TODO(), instance)
-	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
 
-	deploy := &appsv1.Deployment{}
-	g.Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
-		Should(gomega.Succeed())
+	// Create the Metadata object and expect the Reconcile and Deployment to be created
+	err = c.Create(ctx, metadataResource)
+	g.Expect(err).NotTo(HaveOccurred())
 
-	// Delete the Deployment and expect Reconcile to be called for Deployment deletion
-	g.Expect(c.Delete(context.TODO(), deploy)).NotTo(gomega.HaveOccurred())
-	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
-	g.Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
-		Should(gomega.Succeed())
+	// Setup some expected things
+	expectedName := types.NamespacedName{
+		Name:      metadataResource.ObjectMeta.Name,
+		Namespace: metadataResource.ObjectMeta.Namespace,
+	}
+	expectedRequest := reconcile.Request{
+		NamespacedName: expectedName,
+	}
+	expectedLabels := map[string]string{
+		"deployment": metadataResource.ObjectMeta.Name + "-deployment",
+	}
 
-	// Manually delete Deployment since GC isn't enabled in the test control plane
-	// g.Expect(c.Delete(context.TODO(), deploy)).To(gomega.Succeed())
+	// After create the Reconcile function should be called with the expected request object
+	g.Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
 
+	// We expect the fakehsm.FindOrCreateRSAKeyPair() to have been called
+	g.Eventually(hsmClient.FindOrCreateRSAKeyPairCallCount, timeout).Should(Equal(1))
+
+	// We expect a Secret to be created
+	secretResource := &corev1.Secret{}
+	getSecretResource := func() error {
+		return c.Get(ctx, expectedName, secretResource)
+	}
+	g.Eventually(getSecretResource).Should(Succeed())
+	g.Expect(secretResource.Data).To(HaveKeyWithValue("metadata.xml", fakeSignedMetadata))
+	g.Expect(secretResource.Data).To(HaveKeyWithValue("entityID", []byte("https://mything/")))
+	g.Expect(secretResource.Data).To(HaveKeyWithValue("postURL", []byte("https://mything/POST")))
+	g.Expect(secretResource.Data).To(HaveKeyWithValue("redirectURL", []byte("https://mything/Redirect")))
+	g.Expect(secretResource.Data).To(HaveKeyWithValue("hsmUser", []byte("hsm-user")))
+	g.Expect(secretResource.Data).To(HaveKeyWithValue("hsmPassword", []byte("hsm-pass")))
+	g.Expect(secretResource.Data).To(HaveKeyWithValue("hsmIP", []byte("10.0.10.100")))
+	g.Expect(secretResource.Data).To(HaveKey("metadataSigningCert"))
+	g.Expect(secretResource.Data).To(HaveKey("metadataSigningKeyLabel"))
+	g.Expect(secretResource.Data).To(HaveKey("metadataSigningTruststore"))
+	g.Expect(secretResource.Data).To(HaveKey("samlSigningCert"))
+	g.Expect(secretResource.Data).To(HaveKey("samlSigningKeyLabel"))
+	g.Expect(secretResource.Data).To(HaveKey("samlSigningTruststore"))
+	g.Expect(secretResource.Data).To(HaveKey("hsmCustomerCA.crt"))
+	// TODO: add the rest of the Secret fields here...
+
+	// We expect a an nginx Deployment to be created with the same name
+	deploymentResource := &appsv1.Deployment{}
+	getDeploymentResource := func() error {
+		return c.Get(ctx, expectedName, deploymentResource)
+	}
+	g.Eventually(getDeploymentResource).Should(Succeed())
+	g.Expect(deploymentResource.Spec.Selector.MatchLabels).To(Equal(expectedLabels))
+	g.Expect(deploymentResource.Spec.Template.Spec.Containers).To(HaveLen(1))
+	g.Expect(deploymentResource.Spec.Template.Spec.Containers[0].Image).To(Equal("nginx"))
+	g.Expect(deploymentResource.Spec.Template.ObjectMeta.Labels).To(Equal(expectedLabels))
+
+	// We expect a Service to be created in same namespace
+	serviceResource := &corev1.Service{}
+	getServiceResource := func() error {
+		return c.Get(ctx, expectedName, serviceResource)
+	}
+	g.Eventually(getServiceResource).Should(Succeed())
+	g.Expect(serviceResource.Spec.Ports).To(HaveLen(1))
+	g.Expect(serviceResource.Spec.Ports[0].Name).To(Equal("http"))
+	g.Expect(serviceResource.Spec.Ports[0].Protocol).To(Equal(corev1.Protocol("TCP")))
+	g.Expect(serviceResource.Spec.Ports[0].Port).To(Equal(int32(80)))
+	g.Expect(serviceResource.Spec.Ports[0].TargetPort).To(Equal(intstr.FromInt(80)))
+	g.Expect(serviceResource.Spec.Selector).To(Equal(expectedLabels))
+	g.Expect(serviceResource.Spec.ClusterIP).NotTo(Equal(corev1.ClusterIPNone))
+	g.Expect(serviceResource.Spec.ClusterIP).NotTo(Equal(""))
+
+	// Update the metadata
+	metadataResourceUpdated := metadataResource
+	metadataResourceUpdated.Spec.Data.OrgName = "updated-org-name"
+	g.Expect(c.Update(ctx, metadataResourceUpdated)).To(Succeed())
+
+	// After updating the Metadata Reconcile should have been called again
+	g.Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
+
+	// and a new serviceResource should exist
+	prevClusterIP := serviceResource.Spec.ClusterIP
+	serviceResource = &corev1.Service{}
+	g.Eventually(getServiceResource).Should(Succeed())
+
+	// We expect the Service ClusterIP to be unchanged
+	g.Expect(serviceResource.Spec.ClusterIP).To(Equal(prevClusterIP))
 }
