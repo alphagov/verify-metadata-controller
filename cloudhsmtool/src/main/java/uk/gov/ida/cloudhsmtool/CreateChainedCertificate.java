b4ff3f4b4bc5c4514248a2f82b421ab74e6c67b4
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2019-06-19 10:34:54 +0100

    Split genrsa cli into three separate apps to gen keyapirs, create a self-signed cert, or create a chained cert

---
+++ cloudhsmtool/src/main/java/uk/gov/ida/cloudhsmtool/CreateChainedCertificate.java
@@ -0,0 +1,95 @@
+package uk.gov.ida.cloudhsmtool;
+
+import org.bouncycastle.asn1.x509.BasicConstraints;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.KeyUsage;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemReader;
+import picocli.CommandLine;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.math.BigInteger;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.time.Period;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(
+        name = "create-chained-cert",
+        description = "Creates a chained certificate for a CloudHSM label"
+)
+public class CreateChainedCertificate extends CreateSelfSignedCertificate implements Callable<Void> {
+
+    @CommandLine.Option(names = {"-parent-cert-base64"}, required = true)
+    protected String parentCertBase64;
+
+    @CommandLine.Option(names = {"-ca-cert"}, defaultValue = "false")
+    protected boolean certAuthority;
+
+    @CommandLine.Option(names = {"-parent-key-label"}, required = true)
+    protected String parentKeyLabel;
+
+    @Override
+    public Void call() throws Exception {
+        super.call();
+        return null;
+    }
+
+    @Override
+    X509Certificate generateCertificate(KeyPair keyPair) throws Exception {
+
+        X509Certificate parentCert = getParentCertificate(parentCertBase64);
+
+        ZonedDateTime now = ZonedDateTime.now();
+        ZonedDateTime expiry = now.plus(Period.ofMonths(expiryMonths));
+
+        X509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(
+                parentCert, // parent cert is the issuer authority
+                new BigInteger(64, new SecureRandom()),
+                Date.from(now.toInstant()),
+                Date.from(expiry.toInstant()),
+                buildSubject(),
+                keyPair.getPublic());
+
+        KeyStore ks = getKeystore();
+        Key parentKey = ks.getKey(this.parentKeyLabel, null);
+        if (!(parentKey instanceof PrivateKey)) {
+            throw new Exception("failed to fetch PrivateKey for parent key " + this.parentKeyLabel);
+        }
+
+        if (certAuthority) {
+            certBuilder.addExtension(Extension.keyUsage, true, new KeyUsage(KeyUsage.keyCertSign));
+            certBuilder.addExtension(Extension.basicConstraints, false, new BasicConstraints(true));
+        } else {
+            certBuilder.addExtension(Extension.keyUsage, true, new KeyUsage(KeyUsage.digitalSignature));
+        }
+
+        ContentSigner signer = new CaviumRSAContentSigner((PrivateKey) parentKey, SIGNING_ALGO_SHA256_RSA);
+
+        return buildX509Certificate(certBuilder, signer);
+    }
+
+    private X509Certificate getParentCertificate(String pemString) throws CertificateException, IOException {
+        StringReader stringReader = new StringReader(pemString);
+        PemReader pemReader = new PemReader(stringReader);
+        PemObject pemObject = pemReader.readPemObject();
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(pemObject.getContent());
+        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+        X509Certificate cert = (X509Certificate) certFactory.generateCertificate(inputStream);
+        return cert;
+    }
+
+}
