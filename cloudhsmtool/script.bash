76e27b0ec2d73cd3101d220ab47668c0239ce157
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2019-07-30 17:19:20 +0100

    Revert "Merge pull request #31 from alphagov/1548_vmc-java-proxy-md"
    This reverts commit 18a99a62647bd7de4b975ba36372160417708f18, reversing
    changes made to 86d43b16b5b1b46e4cc98b2d6156e9512372bc85.

--- cloudhsmtool/script.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/opt/cloudhsm/bin/cloudhsm_client /opt/cloudhsm/etc/cloudhsm_client.cfg > cloudhsm.log 2>&1 &
-bash
--- mdgen/docker/start.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=/opt/cloudhsm/lib
-export HSM_PARTITION=PARTITION_1
-/opt/cloudhsm/bin/cloudhsm_client /opt/cloudhsm/etc/cloudhsm_client.cfg > cloudhsm.log 2>&1 &
-cd /integration_tests
-gem install bundler
-bundle install
-bundle exec cucumber features/feature/
-bash
