bd93196e0b4228136743a9c0eeed5326e854959a
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2019-07-31 11:14:40 +0100

    EID-1549 Make `launch-docker.bash` more resiliant
    If the `mdgen/test` dir didn't exist `cp` would fail. A second run of
    the script would then succeed. This enforces that it is empty and it
    exists.
    
    Also sets some options to fail early if anything goes wrong rather than
    allowing the integration tests to continue.

--- mdgen/docker/launch-docker.bash
+++ mdgen/docker/launch-docker.bash
@@ -1,11 +1,18 @@
 #!/bin/bash
+
+set -e pipefail
+
 if [ ! -f ../build/distributions/mdgen.zip ] ; then
     pushd ../ > /dev/null
     ./gradlew distZip
     popd
 fi
 cp ../build/distributions/mdgen.zip ./
+
+if [ -d ./test ] ; then rm -fr ./test ; fi
+mkdir ./test
 cp -f ../test/* ./test/
-docker run -it -e HSM_USER -e HSM_PASSWORD -v $(pwd)/test:/test -v $(pwd)/mdgen.zip:/mdgen.zip --mount src=$(pwd)/integration_tests,target=/integration_tests,type=bind mdgen:docker
+
+docker run -it -e HSM_USER -e HSM_PASSWORD -v "$(pwd)"/test:/test -v "$(pwd)"/mdgen.zip:/mdgen.zip --mount src="$(pwd)"/integration_tests,target=/integration_tests,type=bind mdgen:docker
 rm -fr ./mdgen
 rm mdgen.zip
