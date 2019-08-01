758684a36c406610315259f9913fdaf15c865e73
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2019-08-01 12:00:08 +0100

    WIP logic for refreshing a cert when it's due to expire

--- pkg/controller/certificaterequest/certificaterequest_controller.go
+++ pkg/controller/certificaterequest/certificaterequest_controller.go
@@ -34,6 +34,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+	"strconv"
+	"time"
 )
 
 var log = logf.Log.WithName("controller")
@@ -86,8 +88,7 @@ type ReconcileCertificateRequest struct {
 
 // Reconcile reads that state of the cluster for a CertificateRequest object and makes changes based on the state read
 // and what is in the CertificateRequest.Spec
-// a Deployment as an example
-// Automatically generate RBAC rules to allow the Controller to read and write Deployments
+// Automatically generate RBAC rules to allow the Controller to read and write CertificateRequests
 // +kubebuilder:rbac:groups=verify.gov.uk,resources=certificaterequests,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=verify.gov.uk,resources=certificaterequests/status,verbs=get;update;patch
 func (r *ReconcileCertificateRequest) Reconcile(request reconcile.Request) (reconcile.Result, error) {
@@ -106,80 +107,74 @@ func (r *ReconcileCertificateRequest) Reconcile(request reconcile.Request) (reco
 		return reconcileResult, err
 	}
 
-	keyLabel := fmt.Sprintf("%s-%s", certReconcileRequest.ObjectMeta.Namespace, certReconcileRequest.ObjectMeta.Name)
-
-	creds, err := hsm.GetCredentials()
+	hsmCredentials, err := hsm.GetCredentials()
 	if err != nil {
 		return reconcileResult, err
 	}
 
 	// find or create certificate Secret
+	keyLabel := fmt.Sprintf("%s-%s", certReconcileRequest.ObjectMeta.Namespace, certReconcileRequest.ObjectMeta.Name)
 	foundSecret := &corev1.Secret{}
 	err = r.Get(context.TODO(), types.NamespacedName{Name: certReconcileRequest.Name, Namespace: certReconcileRequest.Namespace}, foundSecret)
-	if err != nil && errors.IsNotFound(err) {
-		_, err = r.hsm.FindOrCreateRSAKeyPair(keyLabel, creds)
-		if err != nil {
+	if isNotFoundError(err) {
+
+		// cert is not present in the secret, so ensure there is a keypair in hsm for cert generation
+		if _, err := r.hsm.FindOrCreateRSAKeyPair(keyLabel, hsmCredentials); err != nil {
 			return reconcileResult, fmt.Errorf("findOrCreateRSAKeyPair(%s): %s", keyLabel, err)
 		}
 
-		req := hsm.CertRequest{
-			CountryCode:      certReconcileRequest.Spec.CountryCode,
-			CommonName:       certReconcileRequest.Spec.CommonName,
-			ExpiryMonths:     certReconcileRequest.Spec.ExpiryMonths,
-			Location:         certReconcileRequest.Spec.Location,
-			Organization:     certReconcileRequest.Spec.Organization,
-			OrganizationUnit: certReconcileRequest.Spec.OrganizationUnit,
-			CACert:           certReconcileRequest.Spec.CACert,
-		}
-
-		// the hsm fn to call
-		createCertFn := r.hsm.CreateSelfSignedCert
-
-		if certReconcileRequest.Spec.CertificateAuthority != nil {
-
-			reqName := types.NamespacedName{
-				Name:      certReconcileRequest.Spec.CertificateAuthority.SecretName,
-				Namespace: certReconcileRequest.Spec.CertificateAuthority.Namespace,
-			}
-
-			// get the cert and label saved as a secret
-			caSecret := &corev1.Secret{}
-			if err := r.Get(context.TODO(), reqName, caSecret); err != nil {
-				return reconcileResult, err
-			}
-
-			if val, ok := caSecret.Data["cert"]; ok {
-				req.ParentCertPEM = string(val)
-			} else {
-				return reconcileResult, fmt.Errorf("could not find 'cert' value in secret")
-			}
-
-			if val, ok := caSecret.Data["label"]; ok {
-				req.ParentKeyLabel = string(val)
-			} else {
-				return reconcileResult, fmt.Errorf("could not find 'label' value in secret")
-			}
-
-			createCertFn = r.hsm.CreateChainedCert
-		}
-
-		cert, err := createCertFn(keyLabel, creds, req)
-
+		cert, err := r.createCert(certReconcileRequest, keyLabel)
 		if err != nil {
-			return reconcileResult, fmt.Errorf("CreateChainedCert(%s): %s", keyLabel, err)
+			return reconcileResult, fmt.Errorf("error with createCert: %s", err)
 		}
 
-		if err := r.saveSecret(certReconcileRequest, cert, keyLabel, creds); err != nil {
+		if err := r.saveSecret(certReconcileRequest, cert, keyLabel, hsmCredentials); err != nil {
 			return reconcileResult, err
 		}
 
 	} else if err != nil {
 		return reconcileResult, err
 	} else {
-		// already exists, update?
+
+		certDueToExpireTime := r.certDueToExpireTime(foundSecret, err, certReconcileRequest)
+		if time.Now().After(certDueToExpireTime) {
+
+			// delete the current secret
+			// TODO investigate delete options propogating to delete other cert secrets
+			if err := r.Delete(context.TODO(), foundSecret); err != nil {
+				return reconcileResult, fmt.Errorf("coud not delete current cert secret: %s", err)
+			}
+
+			// create a new cert signed by current hsm keypair
+			cert, err := r.createCert(certReconcileRequest, keyLabel)
+			if err != nil {
+				return reconcileResult, fmt.Errorf("error with createCert: %s", err)
+			}
+
+			// create a new secret containing new cert
+			if err := r.saveSecret(certReconcileRequest, cert, keyLabel, hsmCredentials); err != nil {
+				return reconcileResult, err
+			}
+		}
 	}
 
+	// TODO investigate decorating the reconcileResult with RequeueAfter so it will ensure being called again in say 1 day (a cheap rescheduling)
 	return reconcileResult, nil
+}
+
+func (r *ReconcileCertificateRequest) certDueToExpireTime(foundSecret *corev1.Secret, err error, certReconcileRequest *verifyv1beta1.CertificateRequest) time.Time {
+	// the cert is present, check if it is expiring
+	var expiryMonths int
+	if exp, ok := foundSecret.Data["expiryMonths"]; ok {
+		expiryMonths, err = strconv.Atoi(string(exp))
+	} else {
+		// update secret with spec expiry
+		foundSecret.Data["expiryMonths"] = []byte(strconv.Itoa(certReconcileRequest.Spec.ExpiryMonths))
+		r.Update(context.TODO(), foundSecret)
+	}
+	// due to expire if date of secret creation plus expiryInMonths minus 7 days is greater than now
+	dueToExpireTime := foundSecret.CreationTimestamp.AddDate(0, expiryMonths, 0).AddDate(0, 0, -7)
+	return dueToExpireTime
 }
 
 func (r *ReconcileCertificateRequest) saveSecret(instance *verifyv1beta1.CertificateRequest, cert []byte, keyLabel string, creds hsm.Credentials) error {
@@ -198,6 +193,7 @@ func (r *ReconcileCertificateRequest) saveSecret(instance *verifyv1beta1.Certifi
 			"hsmPassword":   []byte(creds.Password),
 			"hsmIP":         []byte(creds.IP),
 			"hsmCustomerCA": []byte(creds.CustomerCA),
+			"expiryMonths":  []byte(strconv.Itoa(instance.Spec.ExpiryMonths)),
 		},
 	}
 
@@ -215,4 +211,68 @@ func (r *ReconcileCertificateRequest) saveSecret(instance *verifyv1beta1.Certifi
 	)
 
 	return nil
+}
+
+func (r *ReconcileCertificateRequest) createCert(certReconcileRequest *verifyv1beta1.CertificateRequest, keyLabel string) ([]byte, error) {
+
+	certRequest := createCertRequest(certReconcileRequest)
+
+	// the hsm fn to call
+	createCertFn := r.hsm.CreateSelfSignedCert
+
+	// if the spec indicates a CertificateAuthority, that is the CA for this cert
+	if certReconcileRequest.Spec.CertificateAuthority != nil {
+
+		reqForCACertSecret := types.NamespacedName{
+			Name:      certReconcileRequest.Spec.CertificateAuthority.SecretName,
+			Namespace: certReconcileRequest.Spec.CertificateAuthority.Namespace,
+		}
+
+		// get the cert and label saved as a secret
+		caSecret := &corev1.Secret{}
+		if err := r.Get(context.TODO(), reqForCACertSecret, caSecret); err != nil {
+			return nil, err
+		}
+
+		if val, ok := caSecret.Data["cert"]; ok {
+			certRequest.ParentCertPEM = string(val)
+		} else {
+			return nil, fmt.Errorf("could not find 'cert' value in secret")
+		}
+
+		if val, ok := caSecret.Data["label"]; ok {
+			certRequest.ParentKeyLabel = string(val)
+		} else {
+			return nil, fmt.Errorf("could not find 'label' value in secret")
+		}
+
+		createCertFn = r.hsm.CreateChainedCert
+	}
+
+	hsmCredentials, err := hsm.GetCredentials()
+	if err != nil {
+		return nil, err
+	}
+
+	cert, err := createCertFn(keyLabel, hsmCredentials, certRequest)
+	if err != nil {
+		return nil, fmt.Errorf("CreateChainedCert(%s): %s", keyLabel, err)
+	}
+	return cert, nil
+}
+
+func isNotFoundError(err error) bool {
+	return err != nil && errors.IsNotFound(err)
+}
+
+func createCertRequest(certReconcileRequest *verifyv1beta1.CertificateRequest) hsm.CertRequest {
+	return hsm.CertRequest{
+		CountryCode:      certReconcileRequest.Spec.CountryCode,
+		CommonName:       certReconcileRequest.Spec.CommonName,
+		ExpiryMonths:     certReconcileRequest.Spec.ExpiryMonths,
+		Location:         certReconcileRequest.Spec.Location,
+		Organization:     certReconcileRequest.Spec.Organization,
+		OrganizationUnit: certReconcileRequest.Spec.OrganizationUnit,
+		CACert:           certReconcileRequest.Spec.CACert,
+	}
 }
