90e3a4d765b32e26bcf79beb05298127d0e0e760
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2019-10-03 15:43:48 +0100

    BAU Move hsm ip resolution out of Dockerfile
    The HSM ip address used by the cloudhsm daemon was being baked in to the
    Docker image. We've found that the call to dig is now returning more
    than one IP address. This change selects the first IP returned whenever
    the image is started which should hopefully make it more resilient.

--- mdgen/docker/start.bash
+++ mdgen/docker/start.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/opt/cloudhsm/lib
 export HSM_PARTITION=PARTITION_1
+hsm_ip_address=$(dig +short a88bb4c07943b11e9bbf30ae9bf7a1ac-aa921f50a00f6c2a.elb.eu-west-2.amazonaws.com | head -n 1)
+/opt/cloudhsm/bin/configure -a "${hsm_ip_address}"
 /opt/cloudhsm/bin/cloudhsm_client /opt/cloudhsm/etc/cloudhsm_client.cfg > cloudhsm.log 2>&1 &
 cd /integration_tests
 gem install bundler
